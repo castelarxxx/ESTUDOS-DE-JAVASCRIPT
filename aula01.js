59c66e333d919d6fe6d0e786feded6171dcf2f48
Hilary Castelar <108029132+castelarxxx@users.noreply.github.com>
2023-05-02 11:48:51 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,25 @@
+
+
+/*var vouAPraia = false//true
+var dinheiro = 150;
+var nome = "hilary";
+var sobreNome = "Castelar"
+console.log ("meu nome é " + nome + sobreNome)
+
+console.log(`esse é o nome da aluna ${nome} ${sobreNome}`)
+console.log ("Olá, mundo"); */
+
+
+
+
+var notaEmFísica = 10
+var notaEmCálculo = 6
+var resultado = true
+console.log('O resultado da soma da nota ' + notaEmFísica +' multiplicado por ' + notaEmCálculo + ' é ' + notaEmFísica *notaEmCálculo + " pontos nas matérias"); 
+
+
+
+var nome = "hilary";
+var sobreNome = "Castelar";
+var idade = 19;
+console.log (`A aluna ${nome} ${sobreNome} tem ${idade} anos de idade `)
